c2ea9f202584d5f56a1297d04e9a8c753b866445
grayolaRSA <gtndanga@gmail.com>
2023-10-05 21:57:20 +0200

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """module for duck typed annotations"""
 
-from typing import Iterable, List, Union, Tuple, Sequence, Any
+from typing import Union, Sequence, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """function to test correct duck notation"""
     if lst:
         return lst[0]
     else:
