d47baee20b56754977b4f66edd0b57e97c8f04fe
grayolaRSA <gtndanga@gmail.com>
2023-10-05 20:08:26 +0200

    update

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module for floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """floor function that returns floor of float"""
+    return math.floor(n)
