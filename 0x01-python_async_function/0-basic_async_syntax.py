77d5bd897b12fb6bc2333a06a24bebc838a0c4c8
grayolaRSA <gtndanga@gmail.com>
2023-10-09 20:04:26 +0200

    update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,16 @@ import random
 
 async def wait_random(max_delay: int = 10):
     """ async delay function """
-    i = random.uniform(0, max_delay)
-    await asyncio.sleep(i)
-    return i
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def main():
+    """ main function to run wait_random function """
+    res = await asyncio.run(wait_random())
+    return res
+
+if __name__ == "__main__":
+    """ initialisation of module """
+    asyncio.run(main())
