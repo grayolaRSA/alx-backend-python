233ae760e4bf51e42beae0104fe6dcac61de60e8
grayolaRSA <gtndanga@gmail.com>
2023-10-10 20:00:02 +0200

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,13 +12,3 @@ async def async_generator() -> float:
         await asyncio.sleep(1)
         rand: float = random.uniform(0, 10)
         yield rand
-
-
-async def main() -> None:
-    """ main function to run async generator function """
-    res: float = async_generator()
-    return res
-
-if __name__ == "__main__":
-    """ initialisation of module """
-    asyncio.run(main())
