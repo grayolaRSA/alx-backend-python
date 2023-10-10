6c8143b054ee83b1c70d500412143c3930373717
grayolaRSA <gtndanga@gmail.com>
2023-10-10 21:49:12 +0200

    update

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,6 +10,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """async comprehension coroutine"""
-    gen: Generator[float, None, None] = async_generator()
-    result: List[float] = [i async for i in gen]
+    # gen: Generator[float, None, None] = async_generator()
+    result: List[float] = [i async for i in async_generator()]
     return result
