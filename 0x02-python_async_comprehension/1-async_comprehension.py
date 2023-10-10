b8746cc92ae9ac0bac648e951125cfc2ad8737b9
grayolaRSA <gtndanga@gmail.com>
2023-10-10 20:53:57 +0200

    update

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""module for async comprehension"""
+
+
+import asyncio
+from typing import List
+async_gen = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async comprehension coroutine"""
+    gen = async_gen()
+    result = []
+    async for i in gen:
+        result.append(i)
+    return result
+
+
+async def main():
+    values = await async_comprehension()
+    print (values)
+
+if __name__ == "__main__":
+    asyncio.run(main())
