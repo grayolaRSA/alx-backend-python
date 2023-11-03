ec3acca02055db2cf3d2556e23827901e45eb274
grayolaRSA <gtndanga@gmail.com>
2023-11-03 19:19:12 +0200

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 
 import unittest
 from functools import wraps
+import utils
 from unittest.mock import Mock, patch, MagicMock
 from utils import access_nested_map, get_json
 from typing import Mapping, Sequence, Optional, Callable, Dict, List
@@ -57,6 +58,39 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        """a method to test memoization"""
+
+        class TestClass:
+
+            def a_method(self) -> int:
+                """a simple method used to test"""
+                return 42
+
+            @utils.memoize
+            def a_property(self) -> Callable:
+                """another simple method used to model a test property"""
+                return self.a_method()
+
+        test_instance = TestClass()
+
+        with patch.object(test_instance, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+
+            # Call the property twice
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+
+            # Assert that a_method is only called once
+            mock_a_method.assert_called_once()
+
+            # Assert that the results are correct
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
 
 
 if __name__ == '__main__':
